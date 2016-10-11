3be879c225ee61bf861c5e47039503fe233400d2
Andy Hayes <andythemanhayes@hotmail.com>
2016-10-11 11:43:51 -0600

    adds redis url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_cable.allowed_request_origins = ['https://andy-action-cable-app.herokuapp.com', 'http://andy-action-cable-app.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
